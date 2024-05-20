89efae6585cbd8f9320261dcedd00c6e5ea64b84
Artymediys <Artymediys@yandex.ru>
2024-05-20 15:51:19 +0300

    added chek_success func in build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,16 +6,29 @@ REGISTRY_URL=""
 LOGIN=""
 PASSWORD=""
 
-# Building Docker-image
-docker build -t $IMAGE_NAME .
+# Function to check the exit status of the last executed command
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed."
+        exit 1
+    fi
+}
 
 # Log in to registry
 echo $PASSWORD | docker login $REGISTRY_URL -u $LOGIN --password-stdin
+check_success "Docker login"
+
+# Building Docker image
+docker build -t $IMAGE_NAME .
+check_success "Docker build"
 
 # Tagging image
 docker tag $IMAGE_NAME $REGISTRY_URL/$IMAGE_NAME:latest
+check_success "Docker tag"
 
 # Pushing image to registry
 docker push $REGISTRY_URL/$IMAGE_NAME:latest
+check_success "Docker push"
 
-echo "Docker image pushed to $REGISTRY_URL/$IMAGE_NAME:latest"
+# If all commands succeed
+echo "Docker image pushed to $REGISTRY_URL/$IMAGE_NAME:latest successfully."
